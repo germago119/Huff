10c2525737fef66243bca147b7bf000ea23e8cfa
germago <roger.andres18@gmail.com>
2018-06-01 22:51:01 -0600

    I'M PUSHING.

--- main.cpp
+++ main.cpp
@@ -17,15 +17,14 @@ using namespace std;
 
 bool DoesFileExist(char *path) {
     ifstream is(path);
-    if (!is)return false;
-    else return true;
+    return !!is;
 }
 
-int main(int argc, char *argv[]) {
+int main() {
 
-	bool salir = true;
 	int opcion;
-	string path_in, path_out;
+    char *path_in = nullptr;
+    char *path_out = nullptr;
 
 	system("cls");
 	cout << "-------Menu Principal------" << endl << endl << endl;
@@ -40,45 +39,35 @@ int main(int argc, char *argv[]) {
 	cout << "Digite el path de salida: ";
 	cin >> path_out;
 
-	auto a = to_string(opcion);
-	auto b = path_in.c_str();
-	auto c = path_out.c_str();
+    string a = to_string(opcion);
 
-	argv[1] = a.c_str();
-		argv[2] = b;
-		argv[3] = c;
+    string b{"1"};
+    string c{"2"};
 
-    if (argc != 4 || (strcmp(argv[1], "-c") && strcmp(argv[1], "-x"))) {
-        cout << "\nUsage:\n\n"
-             << "-c for creating a compressed file\n\t"
-             << argv[0] << " -c original_file_path compressed_file_path\n\n"
-             << "-x for extracting a file from the compressed file\n\t"
-             << argv[0] << " -x compressed_file_path decompressed_file_path\n";
-        return 0;
-    }
 
-    if (strcmp(argv[1], "-c") == 0) {
-        if (DoesFileExist(argv[2])) {
+    if (a == b) {
+        if (DoesFileExist(path_in)) {
             time_t start, end;
             start = clock();
             HuffmanCode hc;
-            hc.Compress(argv[2], argv[3]);
+            hc.Compress(path_in, path_out);
             end = clock();
             cout << "Done! time: " << setprecision(2) << (end - start) / CLOCKS_PER_SEC << " seconds\n";
         } else {
-            cout << "Error: " << argv[2] << " doesn't exist!\n";
+            cout << "Error: " << path_in << " doesn't exist!\n";
         }
-    } else if (strcmp(argv[1], "-x") == 0) {
-        if (DoesFileExist(argv[2])) {
+    } else if (a == c) {
+        if (DoesFileExist(path_in)) {
             time_t start, end;
             start = clock();
             HuffmanCode hc;
-            hc.Decompress(argv[2], argv[3]);
+            hc.Decompress(path_in, path_out);
             end = clock();
             cout << "Done! time: " << setprecision(2) << (end - start) / CLOCKS_PER_SEC << " seconds\n";
         } else {
-            cout << "Error: " << argv[2] << " doesn't exist!\n";
+            cout << "Error: " << path_in << " doesn't exist!\n";
         }
     }
+
     return 0;
 }
