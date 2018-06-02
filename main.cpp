af19ae2602ec8c8873be478c3321cf3e024a9609
germago <roger.andres18@gmail.com>
2018-06-01 22:16:53 -0600

    Trust me, I'm an engineer!... What the f*ck did just happened here?

--- main.cpp
+++ main.cpp
@@ -1,157 +1,58 @@
-// Patrick Sheehan
+/*
+ * main.cpp
+ *
+ *  Created on: 2016��8��18��
+ *      Author: zhenlin
+ */
 
-#include "huff.h"
-#include <iostream>
-#include <cstring>
+#include"HuffmanCode.h"
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstring> // for strcmp
+#include<ctime>
+#include<iomanip> // for setprecision
 
-void compress();
+using namespace std;
 
-void putOut();
-
-void decompress();
-
-Node *constructHeap();
-
-unsigned int frequencies[256] = {0};
-string codebook[256];
+bool DoesFileExist(char *path) {
+    ifstream is(path);
+    if (!is)return false;
+    else return true;
+}
 
 int main(int argc, char *argv[]) {
-	bool salir = false;
-	int opcion = 0;
+    if (argc != 4 || (strcmp(argv[1], "-c") && strcmp(argv[1], "-x"))) {
+        cout << "\nUsage:\n\n"
+             << "-c for creating a compressed file\n\t"
+             << argv[0] << " -c original_file_path compressed_file_path\n\n"
+             << "-x for extracting a file from the compressed file\n\t"
+             << argv[0] << " -x compressed_file_path decompressed_file_path\n";
+        return 0;
+    }
 
-	while (salir) {
-		system("cls");
-		cout << "-------Menu Principal------" << endl << endl << endl;
-		cout << "1. Comprimir" << endl;
-		cout << "2. Descomprimir" << endl;
-		cout << "Digite una opcion: ";
-		cin >> opcion;
-
-		switch (opcion) {
-		case 1:
-			compress();
-			system("pause");
-			break;
-		case 2:
-			decompress();
-			system("pause");
-			break;
-		}
-	}
-
-   /* if (argc == 2) {
-        if ((argv[1][0] == '-') && (argv[1][1] == 'd'))
-            decompress();
-    } else
-        compress(); */
+    if (strcmp(argv[1], "-c") == 0) {
+        if (DoesFileExist(argv[2])) {
+            time_t start, end;
+            start = clock();
+            HuffmanCode hc;
+            hc.Compress(argv[2], argv[3]);
+            end = clock();
+            cout << "Done! time: " << setprecision(2) << (end - start) / CLOCKS_PER_SEC << " seconds\n";
+        } else {
+            cout << "Error: " << argv[2] << " doesn't exist!\n";
+        }
+    } else if (strcmp(argv[1], "-x") == 0) {
+        if (DoesFileExist(argv[2])) {
+            time_t start, end;
+            start = clock();
+            HuffmanCode hc;
+            hc.Decompress(argv[2], argv[3]);
+            end = clock();
+            cout << "Done! time: " << setprecision(2) << (end - start) / CLOCKS_PER_SEC << " seconds\n";
+        } else {
+            cout << "Error: " << argv[2] << " doesn't exist!\n";
+        }
+    }
     return 0;
-}
-
-void compress() {
-    unsigned char nextChar;
-    // first, calculate the frequencies of each character
-    cin >> noskipws;
-    while (cin >> nextChar)
-        frequencies[nextChar]++;
-
-
-    Node *root = constructHeap();
-    string code;
-    root->fillCodebook(codebook, code);
-
-    putOut();
-}
-
-void putOut() {
-    cout << "HUFFMA3" << '\0';
-
-    unsigned int i;
-    for (i = 0; i < 256; i++) {
-        cout << (char) (0x000000ff & frequencies[i]);
-        cout << (char) ((0x0000ff00 & frequencies[i]) >> 8);
-        cout << (char) ((0x00ff0000 & frequencies[i]) >> 16);
-        cout << (char) ((0xff000000 & frequencies[i]) >> 24);
-    }
-
-    unsigned char nextChar;
-    char nextByte = 0;
-    int bitCounter = 0;
-
-    cin.clear();
-    cin.seekg(0);
-    cin >> noskipws;
-    while (cin >> nextChar) {
-        for (i = 0; i < codebook[nextChar].size(); i++, bitCounter++) {
-            if (bitCounter == 8) {
-                cout << nextByte;
-                nextByte = 0;
-                bitCounter = 0;
-            }
-            if (codebook[nextChar][i] == '1')
-                nextByte = nextByte | (0x01 << bitCounter);
-        }
-    }
-    if (bitCounter)
-        cout << nextByte;
-}
-
-void decompress() {
-    cin >> noskipws;
-    char magic[8];
-    cin.read(magic, 8);
-    char nextByte;
-    for (int i = 0; i < 256; i++) {
-        cin.read((char *) &frequencies[i], 4);
-    }
-
-    Node *root = constructHeap();
-    string code;
-    root->fillCodebook(codebook, code);
-
-    while (cin >> nextByte) {
-        for (int i = 0; i < 8; i++) {
-            if ((nextByte >> i) & 0x01)
-                code += '1';
-            else
-                code += '0';
-            for (int i = 0; i < 256; i++) {
-                if (codebook[i] == code) {
-                    if (frequencies[i]) {
-                        cout << (unsigned char) i;
-                        code.clear();
-                        frequencies[i]--;
-                        break;
-                    } else
-                        return;
-                }
-            } // for
-        }
-    }
-
-
-}
-
-Node *constructHeap() {
-    Heap minHeap;
-    Node *nextNode;
-    for (int i = 0; i < 256; i++) {
-        if (frequencies[i]) {
-            nextNode = new Node(i, frequencies[i]);
-            minHeap.push(nextNode);
-        }
-    }
-
-    Node *node1;
-    Node *node2;
-    Node *merged;
-    while (minHeap.size() > 1) {
-        node1 = minHeap.top();
-        minHeap.pop();
-        node2 = minHeap.top();
-        minHeap.pop();
-        merged = new Node(node1, node2);
-        minHeap.push(merged);
-    }
-
-    return minHeap.top();
 }
